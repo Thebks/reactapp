908f2698ffbce3df8045b44f5d06f05acdb0a74e
Thebks <ichbks@gmail.com>
2022-09-02 00:27:30 +0500

    Wip Brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './brand.css';
+//import { google, slack } from './imports';
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
 
 const Brand = () => {
     return (
-        <div>Brand</div>
+        <div className="reactapp__brand section__padding">
+            <div>
+                <img src={google} alt='google' />
+            </div>
+            <div>
+                <img src={slack} alt='slack' />
+            </div>
+
+        </div>
     )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+
+export {
+    google,
+    slack
+}
