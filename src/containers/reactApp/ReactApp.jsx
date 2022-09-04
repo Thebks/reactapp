b19e53320c2455d2487ad8f18a00fae49523daec
Thebks <ichbks@gmail.com>
2022-09-04 14:47:53 +0500

    Scroll

--- src/containers/reactApp/ReactApp.jsx
+++ src/containers/reactApp/ReactApp.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './reactApp.css';
 
 const ReactApp = () => {
     return (
-        <div>ReactApp</div>
+        <div className='reactapp__reactapp section__margin' id='reactapp'>
+            <div className='reactapp_reactapp-feature'>
+                <Feature />
+            </div>
+            <div className='reactapp__reactapp-heading'>
+                <h1 className='gradient__text'>The possibilities are endless</h1>
+                <p>Explore the Library</p>
+            </div>
+            <div className='reactapp__reactapp-container'>
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
+        </div>
     )
 }
 
