bf21e3acee535d204380c024947f63d7bc7c45de
Thebks <ichbks@gmail.com>
2022-09-05 12:37:19 +0500

    WIP feature component

--- src/containers/reactApp/ReactApp.jsx
+++ src/containers/reactApp/ReactApp.jsx
@@ -6,16 +6,16 @@ const ReactApp = () => {
     return (
         <div className='reactapp__reactapp section__margin' id='reactapp'>
             <div className='reactapp_reactapp-feature'>
-                <Feature />
+                <Feature title={'What is ReactApp'} text={'React is a JavaScript library created by Facebook. React is a User Interface (UI) library. React is a tool for building UI components'} />
             </div>
             <div className='reactapp__reactapp-heading'>
                 <h1>The possibilities are endless</h1>
                 <p>Explore the Library</p>
             </div>
             <div className='reactapp__reactapp-container'>
-                <Feature />
-                <Feature />
-                <Feature />
+                <Feature title='Babel' text={'Babel is a JavaScript compiler that can translate markup or programming languages into JavaScript. With Babel, you can use the newest features of JavaScript (ES6 - ECMAScript 2015).Babel is available for different conversions. React uses Babel to convert JSX into JavaScript.'} />
+                <Feature title={'KnowledgeBase'} text={'Knowledge is the key to work with ReactApp so its never to late to start'} />
+                <Feature title={'Education'} text={'You gotta know shit to do shit so keep learning'} />
             </div>
         </div>
     )
