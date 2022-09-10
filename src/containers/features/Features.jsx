7d1f6e24c8bbcdd7e33f6589b37c655e0e29c23c
Thebks <ichbks@gmail.com>
2022-09-10 12:43:18 +0500

    WIP Feature component

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import './features.css';
+import Feature from '../../components/feature/Feature';
+
+const reactAppFeatures = [
+    {
+        title: 'Code Improvment',
+        text: 'ReactApp offers a variety of coding lessons starting from beginners to professionals. It offers thousands of coding topics to learn coding, brush up your skills or remain are of the latest trends in the coding market'
+    },
+    {
+        title: 'Become a pro',
+        text: "If you're wondering how to become a coder in three to six months and have no coding, web development or software engineering experience, you'll want to enroll in a coding bootcamp."
+    },
+    {
+        title: 'Auto Correct Feature',
+        text: 'ReactApp Lint, is a tool that analyzes source code to flag programming errors, bugs, stylistic errors, and suspicious constructs.'
+    },
+    {
+        title: 'Always Free',
+        text: 'Always Free as the title says'
+    },
+];
 
 const Features = () => {
     return (
-        <div>Features</div>
+        <div className='reactapp__features section__padding'>
+            <div className='reactapp__features-heading'>
+                <h1 className='gradient__text'> The power of ReactApp is infinite</h1>
+                <p>Request early access</p>
+            </div>
+            <div className='reactapp_feature-container'>
+                {reactAppFeatures.map((item, index) => (
+                    <Feature title={item.title} text={item.text} key={item.title + index} />
+                ))}
+            </div>
+        </div>
     )
 }
 
