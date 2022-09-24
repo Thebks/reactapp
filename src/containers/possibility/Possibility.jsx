d3ba6c6949e3183dc36f76794fae53f62118be34
Thebks <ichbks@gmail.com>
2022-09-24 21:04:39 +0500

    WIP possibility

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,7 +8,12 @@ const Possibility = () => {
             <div className='reactapp__possibility-image'>
                 <img src={PossibilityImage} alt='Possibility' />
             </div>
-
+            <div className='reactapp__possibility-content'>
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className='gradient__text'>Like I said The Possibilities are endless</h1>
+                <p>If you think coding his hard then you might be right since you never tried it and because of that you think its hard. Try to print "Hello World" to the console in JavaScript and you will know that its not that hard</p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
         </div>
     )
 }
